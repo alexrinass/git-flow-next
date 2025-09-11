f2d2425d0d0a75920e390387923239d40c4eb565
Alexander Rinass <alex@git-tower.com>
2025-09-11 16:30:33 +0200

    feat: Update version to 0.1.0-rc.1 for pre-release
    Advance from alpha to release candidate status, indicating
    feature-complete and production-ready preview of 0.1.0 final release.

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 // Version information
 const (
 	// Version is the current version of git-flow-next
-	Version = "0.1.0-alpha.2"
+	Version = "0.1.0-rc.1"
 
 	// BuildTime will be injected during build
 	BuildTime = ""
